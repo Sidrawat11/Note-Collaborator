6a4f552d0621f45faa7a7e4f58d0b7dc028aa8f1
Parth Rawat <63117288+Sidrawat11@users.noreply.github.com>
2024-04-11 01:14:57 -0700

    Added some animations to the app

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,36 +3,45 @@ import Sidebar from './Sidebar';
 import Logo from '../Images/Logo.png';
 import userLogo from '../Images/userLogo.svg';
 import hamburger from '../Images/hamLogo.svg';
-import leftArrow from '../Images/h-Close.svg';
+
 
 export default function Header() {
     const [showSideBar, setSideBarShow] = useState(false);
-    const [showUserName, setShowUserName] = useState(false);
+    const [showUser, setShowUser] = useState(false);
 
     const handleChange = () => {
         setSideBarShow(prevState => !prevState);
+        console.log(showUser);
+    };
+
+    const handleDisplayEnter = () => {
+        setShowUser(prevState => !prevState);
+    };
+
+    const handleDisplayExit = () => {
+        setShowUser(prevState => !prevState);
     };
 
     return (
         <div className='flex'>
             <div className={`relative inset-y-0 left-0 transition ease-in-out transform ${showSideBar ? "translate-x-0" : "-translate-x-full"}`}>
-                {showSideBar && <Sidebar />}
+                {showSideBar && <Sidebar sideBar={showSideBar} setSideBarShow={setSideBarShow} />}
             </div>
             <div className={`w-full transform ${showSideBar ? "ml-auto" : "ml-0"} transition delay-200 ease-in-out duration-1000`}>
                 <header className="flex items-center justify-between max-h-16 bg-neutral-100 drop-shadow-lg pe-2 ps-0 py-2">
-                    <img
-                        src={showSideBar ? leftArrow : hamburger}
+                    {!showSideBar && <img
+                        src={hamburger}
                         alt='Sidebar Menu'
                         className='h-10 hover:bg-neutral-200'
                         onClick={handleChange}
-                    />
+                    />}                    
                     <div className="flex justify-center items-center gap-1">
                         <img src={Logo} alt='Logo for the APP' className='h-10' />
                         <span className=''>A Notes Collaborator</span>
                     </div>
-                    <div className="flex justify-center items-center gap-1 bg-gray-300 p-2 rounded-full">
+                    <div className={`flex justify-center items-center gap-1 bg-gray-300 p-2 rounded-full transition-all duration-500 ease-in-out transform hover:scale-105 ${showUser ? "min-w-32" : "min-w-10"}`} onMouseEnter={handleDisplayEnter} onMouseLeave={handleDisplayExit} style={{ transition: 'width 0.5s ease-in-out' }}>
                         <img src={userLogo} alt='User' className='h-6' />
-                        <span className='text-lg'>UserName</span>
+                        {showUser && <span className='text-md'>UserName</span>}
                     </div>
                 </header>
             </div>
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,7 +1,24 @@
-export default function Sidebar(){
-    return (
-       <nav className="flex flex-col h-screen max-w-80 min-w-80 bg-neutral-100 b m-0 drop-shadow-lg">
-           
+import leftArrow from '../Images/h-Close.svg';
+export default function Sidebar({showSideBar , setSideBarShow}){
+    const handleChange = () => {
+        setSideBarShow(prevState => !prevState)
+    }
+    return (       
+       <nav className="flex flex-col h-screen max-w-80 min-w-80 bg-neutral-100 m-0 drop-shadow-lg ">
+           <div className='min-h-16 flex items-center justify-between p-2'>
+                <span>UserName</span>
+                <img
+                        src={leftArrow}
+                        alt='Sidebar Menu'
+                        className='h-10 hover:bg-neutral-200'
+                        onClick={handleChange}
+                    />
+           </div>
+           <ul className='p-2'>
+             <li>Home</li>
+             <li>Notes</li>
+             <li>Workspace</li>
+           </ul>
        </nav> 
     )
 }
